b29245a530771a2d9731a4e60ddc28ffb9ecef19
MoisesFlores22 <moises.flores@wizeline.com>
2020-02-22 02:05:55 -0600

    creation of input component

---
+++ src/components/Input.vue
@@ -0,0 +1,27 @@
+<template>
+  <input
+    class='answer-input'
+    id="answerInput"
+    @input="(e) => updateInput(e.target.value)"
+    type="text"
+  >
+</template>
+
+<script>
+export default {
+  methods: {
+    updateInput(value) {
+      const input = document.getElementById('answerInput');
+      input.value = '';
+      this.$emit('updateInput', value);
+    },
+  },
+};
+</script>
+
+<style>
+.answer-input {
+  display: flex;
+  margin: 0 auto;
+}
+</style>
