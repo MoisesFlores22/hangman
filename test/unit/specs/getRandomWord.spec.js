387c426b584a5c312504e6e5be0001f8fecc49e5
MoisesFlores22 <moises.flores@wizeline.com>
2020-02-23 22:44:03 -0600

    create get random word function test

---
+++ test/unit/specs/getRandomWord.spec.js
@@ -0,0 +1,18 @@
+import getRandomWord from '@/utils/getRandomWord';
+
+describe('gerRandomWord.js', () => {
+  it('should get word of maximum 3 letters if difficulty is set to easy', () => {
+    const word = getRandomWord('easy')
+    expect(word.length).toBeLessThanOrEqual(3);
+  });
+
+  it('should get word of maximum 5 letters if difficulty is set to medium', () => {
+    const word = getRandomWord('medium')
+    expect(word.length).toBeLessThanOrEqual(5);
+  });
+
+  it('should get word of maximum 9 letters if difficulty is set to hard', () => {
+    const word = getRandomWord('hard')
+    expect(word.length).toBeLessThanOrEqual(9);
+  })
+})
