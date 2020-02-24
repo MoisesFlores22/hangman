2dd497023c8e1fbcbe7005e1897b8202de29ae2d
MoisesFlores22 <moises.flores@wizeline.com>
2020-02-23 22:25:20 -0600

    implement constants messages file

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+const GAME_LOST = 'lost';
+const GAME_WIN = 'win';
+const CONTINUE_GAME_MESSAGE = 'If you want to play again press the button';
+const RETURN_TO_MAIN_SCREEN = 'Return to Main Screen';
+const CONGRATS_MESSAGE = 'CONGRATULATIONS';
+const FAILED_MESSAGE = 'Sorry you didn\'t guess the correct word';
+
+export {
+  GAME_LOST,
+  GAME_WIN,
+  CONTINUE_GAME_MESSAGE,
+  RETURN_TO_MAIN_SCREEN,
+  CONGRATS_MESSAGE,
+  FAILED_MESSAGE,
+};
