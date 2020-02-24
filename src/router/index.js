4342a2cbff9233aaa6ba76156b4a8e10e2a4fcd5
MoisesFlores22 <moises.flores@wizeline.com>
2020-02-23 22:26:45 -0600

    export end game page in router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/pages/Home';
 import NewGame from '@/pages/NewGame';
+import EndGame from '@/pages/EndGame';
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/new-game/:level',
       name: 'New Game',
       component: NewGame,
+    },
+    {
+      path: '/end-game/:result',
+      name: 'End Game',
+      component: EndGame,
     },
   ],
 });
