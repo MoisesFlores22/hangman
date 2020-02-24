3f7da4f1b3505efc32235d73ffa11f2b084751c4
MoisesFlores22 <moises.flores@wizeline.com>
2020-02-23 23:31:33 -0600

    implement lint fixes in specs

--- test/unit/specs/getLetterAparitions.spec.js
+++ test/unit/specs/getLetterAparitions.spec.js
@@ -1,16 +1,17 @@
 import getLetterAparitions from '@/utils/getLetterAparitions';
-const testArray = ['a', 'd', 'd']
+
+const testArray = ['a', 'd', 'd'];
 
 describe('getLetterAparitions.js', () => {
   describe('letter appartions', () => {
     it('should be 2 appartions', () => {
-      const resultArray = getLetterAparitions(testArray, 'd')
+      const resultArray = getLetterAparitions(testArray, 'd');
       expect(resultArray.length).toEqual(2);
     });
 
     it('should hav 0 apparitions', () => {
-      const resultArray = getLetterAparitions(testArray, 'f')
+      const resultArray = getLetterAparitions(testArray, 'f');
       expect(resultArray.length).toEqual(0);
     });
-  })
-})
+  });
+});
--- test/unit/specs/getRandomWord.spec.js
+++ test/unit/specs/getRandomWord.spec.js
@@ -2,17 +2,17 @@ import getRandomWord from '@/utils/getRandomWord';
 
 describe('gerRandomWord.js', () => {
   it('should get word of maximum 3 letters if difficulty is set to easy', () => {
-    const word = getRandomWord('easy')
+    const word = getRandomWord('easy');
     expect(word.length).toBeLessThanOrEqual(3);
   });
 
   it('should get word of maximum 5 letters if difficulty is set to medium', () => {
-    const word = getRandomWord('medium')
+    const word = getRandomWord('medium');
     expect(word.length).toBeLessThanOrEqual(5);
   });
 
   it('should get word of maximum 9 letters if difficulty is set to hard', () => {
-    const word = getRandomWord('hard')
+    const word = getRandomWord('hard');
     expect(word.length).toBeLessThanOrEqual(9);
-  })
-})
+  });
+});
