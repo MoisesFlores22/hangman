7fdbaa71828728716b16c8efe0ea4af0a38071f6
MoisesFlores22 <moises.flores@wizeline.com>
2020-02-23 22:44:36 -0600

    create get letter aparitions function test

---
+++ test/unit/specs/getLetterAparitions.spec.js
@@ -0,0 +1,16 @@
+import getLetterAparitions from '@/utils/getLetterAparitions';
+const testArray = ['a', 'd', 'd']
+
+describe('getLetterAparitions.js', () => {
+  describe('letter appartions', () => {
+    it('should be 2 appartions', () => {
+      const resultArray = getLetterAparitions(testArray, 'd')
+      expect(resultArray.length).toEqual(2);
+    });
+
+    it('should hav 0 apparitions', () => {
+      const resultArray = getLetterAparitions(testArray, 'f')
+      expect(resultArray.length).toEqual(0);
+    });
+  })
+})
