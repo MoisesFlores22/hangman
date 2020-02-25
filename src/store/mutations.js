acba532b9096911ba29a7803e9070905a7259936
MoisesFlores22 <moises.flores@wizeline.com>
2020-02-24 18:14:47 -0600

    implement save to local storage in mutations

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,9 +1,23 @@
 const mutations = {
   incrementWin(state) {
     state.wins += 1;
+    if (localStorage.getItem('wins')) {
+      this.replaceState(
+        Object.assign(state.wins, JSON.parse(localStorage.getItem('wins'))),
+      );
+    } else {
+      localStorage.setItem('wins', JSON.stringify(state.wins));
+    }
   },
   incrementLose(state) {
     state.loses += 1;
+    if (localStorage.getItem('loses')) {
+      this.replaceState(
+        Object.assign(state.wins, JSON.parse(localStorage.getItem('loses'))),
+      );
+    } else {
+      localStorage.setItem('loses', JSON.stringify(state.loses));
+    }
   },
 };
 
